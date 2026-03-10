951c59755c960999161e0221c32f90e8a06c3a05
SahanPremarathna <sahanpremarathna2005@gmail.com>
2026-03-10 14:09:50 +0530

    Splash Screen Optimization

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,7 +1,29 @@
 import { initGoalsAnimation } from "./home/goals.js";
 import { initHeroScale } from "./home/hero-scale.js";
 
+function initHeroVideoLoadState() {
+    var heroSection = document.querySelector(".hero-section");
+    var heroVideo = document.querySelector(".hero-video");
+
+    if (!heroSection || !heroVideo) {
+        return;
+    }
+
+    var markVideoReady = function () {
+        heroSection.classList.add("video-ready");
+    };
+
+    if (heroVideo.readyState >= 2) {
+        markVideoReady();
+        return;
+    }
+
+    heroVideo.addEventListener("loadeddata", markVideoReady, { once: true });
+    heroVideo.addEventListener("canplay", markVideoReady, { once: true });
+}
+
 initGoalsAnimation();
 initHeroScale();
+initHeroVideoLoadState();
 
 
